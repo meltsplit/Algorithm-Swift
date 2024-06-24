90bb45571af4fe8f0f7c59c1ab823f899ad671a9
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-06-24 18:04:30 +0900

    Update 모음 사전.swift

--- 프로그래머스/2/84512. 모음 사전/모음 사전.swift
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -1,4 +1,22 @@
 func solution(_ word:String) -> Int {
+    var aeiou = "AEIOU".map { String($0) }
+    var answer: [String] = []
+
+    func dfs(_ str: String) {
+        guard str.count <= 5 else { return }
+        answer.append(str)
+        for char in aeiou {
+            dfs(str+char)
+        }
+        
+    }
+    dfs("")
+
+    return Int(answer.firstIndex(of: word)!)
+}
+// 출처: https://developer-eddy403.tistory.com/131 [@EddiOS:티스토리]
+
+func solution(_ word:String) -> Int {
     var word = word.map { String($0) }
     var dict = ["A": 1, "E": 2, "I": 3, "O": 4, "U": 5]
     var nums = [781, 156, 31, 6, 1]
