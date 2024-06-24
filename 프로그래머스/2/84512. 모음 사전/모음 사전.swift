59310bcf80fe99b87a0d8bb2eb93fafcd5b311a2
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-06-24 14:13:36 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -0,0 +1,15 @@
+func solution(_ word:String) -> Int {
+    var word = word.map { String($0) }
+    var dict = ["A": 1, "E": 2, "I": 3, "O": 4, "U": 5]
+    var nums = [781, 156, 31, 6, 1]
+    var sum = 0
+
+    for i in 0..<word.count {
+        if let n = dict[word[i]] {
+            sum += nums[i] * (n-1) + 1
+        }
+    }
+
+    return sum
+}
+// 출처: https://developer-eddy403.tistory.com/131 [@EddiOS:티스토리]
