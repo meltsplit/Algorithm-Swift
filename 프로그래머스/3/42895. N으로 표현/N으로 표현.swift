3f6ae46d99df0da1d3b734e1790a5887fcaeca79
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-06-30 16:27:44 +0900

    [level 3] Title: N으로 표현, Time: 14.56 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.swift
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.swift
@@ -1,27 +1,72 @@
-import Foundation
-
-func dfs(_ N: Int, _ number: Int, _ depth: Int, _ temp: Int, _ answer: inout Int)  {
-    if depth > 8 {
-        return
-    }
-
-    if temp == number && (answer > depth || answer == -1) {
-        answer = depth
-    }
-
-    var calc = 0
-
-    for index in 0 ..< 8 {
-        calc = calc * 10 + N
-        dfs(N, number, depth + 1 + index, temp + calc, &answer)
-        dfs(N, number, depth + 1 + index, temp - calc, &answer)
-        dfs(N, number, depth + 1 + index, temp * calc, &answer)
-        dfs(N, number, depth + 1 + index, temp / calc, &answer)
-    }
+// 사칙 연산 정의
+func add(_ x: Int, _ y: Int) -> Int {
+    x + y
 }
-func solution(_ N:Int, _ number:Int) -> Int {
 
-    var answer = -1
-    dfs(N, number, 0, 0, &answer)
-    return answer
+func substract(_ x: Int, _ y: Int) -> Int {
+    x - y
+}
+
+func multiply(_ x: Int, _ y: Int) -> Int {
+    x * y
+}
+
+func divide(_ x: Int, _ y: Int) -> Int {
+    x / y
+}
+
+func solution(_ N:Int, _ number:Int) -> Int {
+    // 두 집합에 있는 숫자들을 사칙연산한 값들을 구하는 함수
+    func operateTwoSets(_ set1: Set<Int>, _ set2: Set<Int>, _ operation: (Int, Int) -> Int) -> Set<Int> {
+        var result = Set<Int>()
+        
+        for x in set1 {
+            for y in set2 {
+                let number = operation(x, y)
+                if number >= 1 && number <= 32000 {
+                    result.insert(number)
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    // 숫자 N을 n개 가지고 만들 수 있는 수를 구하는 함수
+    func canMakeNumbers(_ n: Int) -> Set<Int> {
+        var result = Set<Int>()
+        let operations = [add, substract, multiply, divide]
+        
+        // 그냥 N을 n개 나열해서 쓴 수
+        result.insert(Int(String(repeating: "\(N)", count: n))!)
+        
+        // 1일 때는 연산할 집합이 없으니까 리턴
+        guard n != 1 else { return result }
+        
+        // set[1] ~ set[n - 1]에서 set[n - 1] ~ set[1]까지 집합을 각각 사칙연산한 결과를 result에 추가
+        for i in 1...(n - 1) {
+            for operation in operations {
+                result = result.union(operateTwoSets(sets[i], sets[n - i], operation))
+            }
+        }
+        return result
+    }
+    
+    // sets[i] = i개의 N으로 만들 수 있는 수
+    var sets = Array(repeating: Set<Int>(), count: 9)
+    
+    // 1 ~ 8개로 구할 수 있는 숫자들을 집합에 넣는다.
+    for i in 1...8 {
+        sets[i] = canMakeNumbers(i)
+    }
+    
+    // number를 만들 수 있는 가장 적은 N을 찾는다
+    for i in 1...8 {
+        if sets[i].contains(number) {
+            return i
+        }
+    }
+    
+    // 8개 이내의 N으로 number를 만들 수 없다면 -1을 리턴한다.
+    return -1
 }
