76efce990e1fb4b2c3fc6e4c39272581ec9fc683
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-07-18 17:12:58 +0900

    [level 3] Title: 입국심사, Time: 725.90 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.swift
+++ 프로그래머스/3/43238. 입국심사/입국심사.swift
@@ -1,17 +1,36 @@
 import Foundation
 
+// IDEA: 이분 탐색
+// t 시간동안 심사관들이 n명의 사람을 처리할 수 있는가?
+// 0 <= t <= 10 * n
+
+// FFFFFFFFTTTTTTTTTTT
+// 최솟값구하기
+
 func solution(_ n:Int, _ times:[Int]) -> Int64 {
-    let maxTime = times.max()!
-    var start = 1
-    var end = maxTime * n
-    var mid = (start + end) / 2
     
-    while start < end {
-        mid = (start + end) / 2
-        let count = times.reduce(0) { $0 + (mid / $1) }
-        start = count < n ? mid + 1 : start
-        end = count < n ? end : mid
+    var low = 0
+    var high = n * times.max()!
+    
+    // time 시간동안 심사관들은 n명을 처리할 수 있는가?
+    func isPossible(_ time: Int) -> Bool {
+        var sum = 0
+        for t in times {
+            let done = time / t 
+            sum += done
+        }
+        
+        return sum >= n
     }
     
-    return Int64(end)
+    while low + 1 < high {
+        let mid = (low + high) / 2
+        if isPossible(mid) {
+            high = mid
+        } else {
+            low = mid
+        }
+    } 
+   
+    return Int64(high)
 }
