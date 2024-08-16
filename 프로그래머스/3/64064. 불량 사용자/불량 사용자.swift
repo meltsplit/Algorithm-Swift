a84a6235085af70e24cdb7e0a358daa3063844a5
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-08-16 18:49:07 +0900

    [level 3] Title: 불량 사용자, Time: 224.36 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.swift
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.swift
@@ -16,31 +16,27 @@ import Foundation
 // 무의미한 경우: fca: cfa , fcf: cff -> Set
 
 func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
-    var dict : [Int: [String]] = [:]
-    var bannedUser : [[String]] = []
-    var answer: Set<Set<String>> = []
-    
-    for user in user_id {
-        dict[user.count, default: []].append(user)
-    }
+    let userDict : [Int: [String]] = Dictionary(grouping: user_id, by: { $0.count })
+    var banList : [[String]] = []
+    var answers: Set<Set<String>> = []
     
     for ban in banned_id {
-        let users = dict[ban.count] ?? []
-        let user = users.filter { zip($0,ban).allSatisfy {$1 == "*" || $0 == $1 }}
-        bannedUser.append(user)
+        let users = userDict[ban.count] ?? []
+        let wilBan = users.filter { zip($0,ban).allSatisfy {$1 == "*" || $0 == $1 }}
+        banList.append(wilBan)
     }
     
     
     func dfs(_ depth: Int, _ visited: Set<String>) {
         var visited = visited
         
-        guard depth < bannedUser.count else { 
-            answer.insert(visited)
+        guard depth < banList.count else { 
+            answers.insert(visited)
             return 
         }
    
         
-        for user in bannedUser[depth] {
+        for user in banList[depth] {
             guard !visited.contains(user) else { continue }
             visited.insert(user)
             dfs(depth + 1, visited)
@@ -51,5 +47,5 @@ func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
     
     dfs(0, [])
     
-    return answer.count
+    return answers.count
 }
