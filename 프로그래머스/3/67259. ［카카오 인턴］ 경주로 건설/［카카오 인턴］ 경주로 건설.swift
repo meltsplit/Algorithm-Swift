db0253a28a0990cc9cd70442ac33182e12b95d73
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-08-18 11:42:48 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 2934.56 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.swift
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.swift
@@ -1,44 +1,68 @@
 import Foundation
 
-func solution(_ board:[[Int]]) -> Int {
-    enum Direction: Int {
-        case down, up, right, left;
-    }
-    let costs = Array(repeating: Array(repeating: 987654321, count: board.count), count: board.count)
-    var dirCosts = Array(repeating: costs, count: 4)
+// 00 28
+// sol: 건설 최소 비용
+
+// Idea 1: 각 인덱스까지 가는 최소 비용
+
+enum Direction: Int, CaseIterable {
+    case up, down, left, right
     
-    for i in 0..<4 {
-        dirCosts[i][0][0] = 0
-    }
-    
-    func isInRange(x: Int, y: Int) -> Bool {
-        return x >= 0 && x < board.count && y >= 0 && y < board.count
-    }
-    
-    func dfs(_ x: Int, _ y: Int, _ currDir: Direction) {
-        let xDir = [0, 0, 1, -1]
-        let yDir = [1, -1, 0, 0]
-        let dirs:[Direction] = [.down, .up, .right, .left]
-        
-        for i in 0..<4 {
-            let nextX = x + xDir[i]
-            let nextY = y + yDir[i]
-            let nextDir = dirs[i]
-            let costNext = dirCosts[currDir.rawValue][x][y] + (currDir == nextDir ? 100 : 600)
-            
-            guard isInRange(x: nextX, y: nextY) == true,
-                  dirCosts[i][nextX][nextY] > costNext else { continue }
-            
-            dirCosts[i][nextX][nextY] = costNext
-            if board[nextX][nextY] != 1 {
-                dfs(nextX, nextY, nextDir)
-            }
+    var dx: Int {
+        switch self {
+            case .up: return -1
+            case .down: return 1
+            case .left: return 0
+            case .right: return 0
         }
     }
-    dfs(0, 0, .up)
-    dfs(0, 0, .left)
-    dfs(0, 0, .right)
-    dfs(0, 0, .down)
     
-    return dirCosts.map({ $0[board.count - 1][board.count - 1] }).min() as! Int
+    var dy: Int {
+        switch self {
+            case .up: return 0
+            case .down: return 0
+            case .left: return -1
+            case .right: return 1
+        }
+    }
+}
+
+func solution(_ board:[[Int]]) -> Int {
+    
+    var costBoard = Array(repeating: Array(repeating: Int.max, count: board.count), count: board.count)
+    var dirBoard = Array(repeating: costBoard, count: 4)
+    
+    for i in 0..<4 {
+        dirBoard[i][0][0] = 0
+    }
+    
+    func isValid(_ x: Int, _ y: Int) -> Bool {
+        return x >= 0 && x < board.count && y >= 0 && y < board.count 
+    }
+    
+    func dfs(_ x: Int, _ y: Int, _ prevDir: Direction) {
+       
+        let dx = [-1, 1, 0, 0]
+        let dy = [0, 0, -1, 1]
+        
+        for i in 0..<4 {
+            let nextX = x + dx[i]
+            let nextY = y + dy[i]
+            let nextDir = Direction(rawValue: i)!
+            let nextCost = dirBoard[prevDir.rawValue][x][y] + (prevDir == nextDir ? 100 : 600)
+             
+            guard isValid(nextX, nextY) else { continue }
+            guard nextCost < dirBoard[i][nextX][nextY] else { continue }
+        
+            dirBoard[i][nextX][nextY] = nextCost
+            guard board[nextX][nextY] == 0 else { continue }
+            dfs(nextX, nextY, nextDir)
+        }
+    }
+    
+    for dir in Direction.allCases {
+        dfs(0,0, dir)
+    }
+    
+    return dirBoard.map { $0[board.count-1][board.count-1] }.min()! 
 }
