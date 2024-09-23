8d889466f1bf51f533003087afc014d9eee73067
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-09-23 19:01:14 +0900

    [level 3] Title: 110 옮기기, Time: 1114.44 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.swift
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.swift
@@ -18,9 +18,8 @@ import Foundation
 // Idea 2: 110을 가장 뒤쪽으로 보낸다.반복 . 그 후 111이 있다면 가장 뒤로 보낸다.
 
 
-
 extension String {
-    func remove110() -> ([String], Int) {
+    func pullBack(_ a: String,_ b: String, _ c: String) -> String {
         var stack = [String]()
         var arr = Array(self.map { String($0) }.reversed())
         var count = 0
@@ -33,9 +32,9 @@ extension String {
             let secondLast = stack.removeLast() 
             let thirdLast = stack.removeLast()
             
-            if thirdLast == "1"
-            && secondLast == "1"
-            && last == "0" {
+            if thirdLast == a
+            && secondLast == b
+            && last == c {
                 count += 1
             } else {
                 stack.append(thirdLast)
@@ -44,42 +43,43 @@ extension String {
             }
             
         }
-        return (stack, count)
-    }
-    
-}
-
-func insert110(_ _stack: [String], _ count: Int) -> String {
-        var stack = _stack
-        var result = [String]()
+        // print("count", count)
+        // print("stack", stack)
+        var finalResult = [String]()
         while !stack.isEmpty {
             let last = stack.removeLast()
-            guard last != "0" 
-            else {
+            if last == "0" {
                 for _ in 0..<count {
-                    result.append(contentsOf: ["0", "1", "1"])
+                    finalResult.append(c)
+                    finalResult.append(b)
+                    finalResult.append(a)
+                    // print("0일때 포문")
                 }
-                result.append(last)
-                result.append(contentsOf: stack.reversed())
+                finalResult.append(last)
+                finalResult.append(contentsOf: stack.reversed())
                 break
+                
             }
-            
-            result.append(last)
+            finalResult.append(last)
         }
+        // print("finalResult", finalResult)
         
-        
-        if result.count != _stack.count + count * 3 {
+        if finalResult.count != self.count {
             for _ in 0..<count {
-                result.append(contentsOf: ["0", "1", "1"])   
-            }
+                    finalResult.append(c)
+                    finalResult.append(b)
+                    finalResult.append(a)
+                }
         }
+        // print("finalResult", finalResult)
         
-        return result.reversed().joined()
+        return finalResult.reversed().joined()
     }
+}
 
 func solution(_ s:[String]) -> [String] {
     return s.map {
-        let (no110Arr, count) = $0.remove110()
-        return insert110(no110Arr, count)
+        // print("---")
+        return $0.pullBack("1", "1", "0")
     } 
 }
