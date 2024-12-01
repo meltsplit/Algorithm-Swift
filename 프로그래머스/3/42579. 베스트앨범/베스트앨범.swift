361d6778f32227b2fcc40ac9ee5258c252ddfe59
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-12-01 23:44:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.53 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -1,18 +1,31 @@
 import Foundation
 
+// 23:17
+// return: 고유번호 순서대로 
+
+struct Music {
+    let id: Int
+    let play: Int
+}
+
 func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
     
-    var g_ids: [String: [Int]] = [:] // 장르에 속한 고유번호 집합
-    var g_play: [String: Int] = [:]
+    var dict: [String: [Music]] = [:]
+    var playDict: [String: Int] = [:]
     
-    for (i, (g, p)) in zip(genres,plays).enumerated() {
-        
-        g_ids[g, default: []] += [i]
-        g_play[g, default: 0] += p
+    for i in genres.indices {
+        let genre = genres[i]
+        let play = plays[i]
+        let music = Music(id: i, play: play)
+        playDict[genre, default: 0] += play
+        dict[genre, default: []].append(music)
     }
     
-    let answer = g_ids.sorted { g_play[$0.key]! > g_play[$1.key]! } 
-                      .map { $0.value.sorted { plays[$0] > plays[$1] }} 
-                      .flatMap { $0[0..<min($0.count, 2)] }
-    return answer
+    var result: [Int] = []
+    for (genre, _) in playDict.sorted { $0.value > $1.value} {
+        guard let musics = dict[genre] else { continue }
+        let top2 = musics.sorted { $0.play > $1.play }.map { $0.id }.prefix(2)
+        result.append(contentsOf: top2)
+    }
+    return result
 }
