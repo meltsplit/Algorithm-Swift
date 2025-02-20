37b7f2fd0fdf52d6a4ac1508b015372ed86cac58
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-02-20 14:53:55 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -1,45 +1,31 @@
 import Foundation
 
-// Idea 1: 유니온 파인드로 서로 다른 부모 몇개인지 구별하기
-// 부모 배열 만들고
-// 포문 돌아가며 find및 유니온 수행
-// 부모 배열에 서로다른 원소 몇개있는 지 coun
 
+// return: 네트워크 개수
+
+// Idea 1: dfs
+// 방문 처리 하면서, 노드가 끝났을 때 answer + 1
+// O(n ^ 2)
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     
-    var parents = (0..<n).map { $0 }
+    var visited = Set<Int>()
+    var answer = 0
     
-    func find(_ a: Int) -> Int {
-        guard a != parents[a] else { return a }
-        parents[a] = find(parents[a])
-        return parents[a]
-    }
-    
-    func union(_ a: Int, _ b: Int) {
-        let parentOfA = find(a)
-        let parentOfB = find(b)
-        
-        guard parentOfA == parentOfA else { return } 
-        
-        if parentOfA < parentOfB {
-            parents[parentOfB] = parentOfA
-        } else {
-            parents[parentOfA] = parentOfB
+    func dfs(_ now: Int, _ visited: inout Set<Int>) {
+        guard !visited.contains(now) else { return } 
+        visited.insert(now)
+        for i in computers[now].indices {
+            guard i != now && computers[now][i] == 1 else { continue }
+            dfs(i, &visited)
         }
     }
     
-    for i in 0..<n {
-        for j in 0..<n {
-            if computers[i][j] == 1 {
-                union(i,j)
-            }
-        }
+        
+    for i in computers.indices {
+        guard !visited.contains(i) else { continue }
+        answer += 1
+        dfs(i, &visited)
     }
     
-    for i in parents.indices {
-        parents[i] = find(i)
-    }
-    
-
-    return Set(parents).count
+    return answer
 }
