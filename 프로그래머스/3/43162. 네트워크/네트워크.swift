8c5cf4fac3f9169f5de57a0dd02840444b3388a0
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-02-20 15:35:50 +0900

    [level 3] Title: 네트워크, Time: 13.88 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -8,24 +8,37 @@ import Foundation
 // O(n ^ 2)
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     
-    var visited = Set<Int>()
-    var answer = 0
+    var parent = (0..<n).map { $0 }
     
-    func dfs(_ now: Int, _ visited: inout Set<Int>) {
-        guard !visited.contains(now) else { return } 
-        visited.insert(now)
-        for i in computers[now].indices {
-            guard i != now && computers[now][i] == 1 else { continue }
-            dfs(i, &visited)
+    func find(_ a: Int) -> Int {
+        guard parent[a] != a else { return a }
+        parent[a] = find(parent[a])
+        return parent[a]
+    }
+    
+    func union(_ a: Int, _ b: Int) {
+        let parentA = find(a)
+        let parentB = find(b)
+        
+        if parentA <= parentB { // b가 a의 부모를 따라가야 함
+            parent[parentB] = parentA
+        } else {
+            parent[parentA] = parentB
         }
     }
     
-        
-    for i in computers.indices {
-        guard !visited.contains(i) else { continue }
-        answer += 1
-        dfs(i, &visited)
+    for i in 0..<n {
+        for j in (i + 1)..<n {
+            guard computers[i][j] == 1 else { continue }
+            print(i,j,"병합")
+            union(i,j)
+            print(parent)
+        }
     }
     
-    return answer
+    for i in 0..<n {
+        find(i)
+    }
+    
+    return Set(parent).count
 }
