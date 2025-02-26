c3e71efb680c1b48bdab67896b0fdf4513d9841f
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-02-26 14:55:49 +0900

    [level 3] Title: 기지국 설치, Time: 1.13 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.swift
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.swift
@@ -1,50 +1,28 @@
 import Foundation
 
-// n: 아파트 개수
-// stations: 아파트 번호 배열 (오름차순 정렬, N보다 작거나 같음)
-// w : 5g 전파 도달 거리
-// RETURN: 기지국 개수의 최솟값
+// 1413
+// return: 기지국 최솟값
+func solution(_ n:Int, _ stations:[Int], _ w:Int) -> Int{
+    var answer = 0 
+    var low = 1
+    var section = [Int]()
 
-// Idea 0: Greedy
-// 채워야 하는 아파트 = 이미 설치된 걸 뺀 아파트들
-// 
-
-func solution(_ n:Int, _ stations:[Int], _ w:Int) -> Int {
-    
-    var lastIndex = 0
-    var answer = 0
-    
     for station in stations {
-        let startIndex = max(station - w, 1)
-        let endIndex = min(station + w, n)
-        
-        guard startIndex > 1 else { 
-            lastIndex = endIndex
-            continue 
-        }
-
-        guard lastIndex < startIndex else {
-            lastIndex = endIndex
-            continue 
-        }
-        
-        guard lastIndex <= n else {
-            lastIndex = endIndex
-            break
-        }
-        
-        let count = startIndex - lastIndex - 1
-        let newStationCount = Int(ceil(Double(count) / Double((w * 2 + 1))))
-        answer += newStationCount
-        lastIndex = endIndex
-    }
-
-    if lastIndex < n {
-        var count = n - lastIndex
-        let newStationCount = Int(ceil(Double(count) / Double((w * 2 + 1))))
-        answer += newStationCount
+        let high = station - (w + 1)
+        let distance = high - low + 1
+        low = station + w + 1
+        guard distance > 0 else { continue }
+        answer += Int(ceil(Double(distance) / Double(2 * w + 1)))
     }
     
+    let high = n
+    let distance = high - low + 1
+    if distance > 0 {
+        answer += Int(ceil(Double(distance) / Double(2 * w + 1)))
+    }
+    
+    
+    
+
     return answer
-    
 }
