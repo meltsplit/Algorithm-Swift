61b1717175e2435b98f566ac554b4a018fec5449
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-02-27 13:36:07 +0900

    [level 3] Title: 스티커 모으기(2), Time: 4.13 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.swift
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.swift
@@ -6,38 +6,23 @@ import Foundation
 func solution(_ sticker:[Int]) -> Int{
     guard sticker.count > 2 else { return sticker.first! }
     
-    var dp1 = Array(repeating: (0, false), count: sticker.count)
-    var dp2 = Array(repeating: (0, false), count: sticker.count)
+    var dp1 = [Int]()
+    var dp2 = [Int]()
     
-    dp1[0] = (sticker.first!, true)
-    dp2[0] = (0, false)
+    dp1.append(sticker.first!)
+    dp1.append(sticker.first!)
     
-    dp1[1] = (sticker.first!, false)
-    dp2[1] = (sticker[1], true)
-    
-    func execute(_ dp: [(Int, Bool)], _ newValue: Int, _ i: Int) -> (Int, Bool) {
-        if !dp[i - 1].1 { 
-            return (dp[i - 1].0 + newValue, true)
-        } else {
-            let a = dp[i - 2].0 + newValue
-            let b = dp[i - 1].0
-            if a > b {
-                return (a, true)
-            } else {
-                return (b, false)
-            }
-        }
-    }
+    dp2.append(0)
+    dp2.append(sticker[1])
+ 
     
     for i in sticker.indices where i > 1 {
         let newValue = sticker[i]
         if i < sticker.count - 1 {
-            dp1[i] = execute(dp1, newValue, i)
-        } else {
-            dp1[i] = dp1[i - 1]
-        }
-        dp2[i] = execute(dp2, newValue, i)
+            dp1.append(max(dp1[i - 2] + newValue, dp1[i - 1]))
+        } 
+        dp2.append(max(dp2[i - 2] + newValue, dp2[i - 1]))
     }
 
-    return max(dp1.last!.0, dp2.last!.0)
+    return max(dp1.last!, dp2.last!)
 }
