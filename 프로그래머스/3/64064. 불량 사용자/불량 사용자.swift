58001b8363a2ee345f0456cac37adfb9d811f552
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-02-28 17:35:19 +0900

    [level 3] Title: 불량 사용자, Time: 604.98 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.swift
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.swift
@@ -1,49 +1,42 @@
 import Foundation
 
-// return 제재 아이디 목록 수
+// 1700
 
-// 17:40 
+// return: 제외할 경우의 수
 
-
-// *rodo: frodo, crodo
-// *rodo: frodo, crodo
-// ******: abc123, frodoc
-
-// frodo crodo abc123 frodoc
-
-// (2 * 2 * 2) - (중복 경우) - (무의미한 경우)
-// 중복 경우: ffa fff ccf cca
-// 무의미한 경우: fca: cfa , fcf: cff -> Set
 
 func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
-    let userDict : [Int: [String]] = Dictionary(grouping: user_id, by: { $0.count })
-    var banList : [[String]] = []
-    var answers: Set<Set<String>> = []
+    var visited = Set<String>()
+    var answers = Set<Set<String>>()
     
-    for ban in banned_id {
-        let users = userDict[ban.count] ?? []
-        let wilBan = users.filter { zip($0,ban).allSatisfy {$1 == "*" || $0 == $1 }}
-        banList.append(wilBan)
-    }
-    
-    
-    func dfs(_ depth: Int, _ visited: Set<String>) {
-        var visited = visited
-        
-        guard depth < banList.count else { 
-            answers.insert(visited)
+    func dfs(_ depth: Int, _ bag: Set<String>) {
+        guard depth < banned_id.count 
+        else { 
+            answers.insert(bag)
             return 
         }
-   
         
-        for user in banList[depth] {
-            guard !visited.contains(user) else { continue }
-            dfs(depth + 1, visited.union([user]))
+        let target = banned_id[depth]
+        let nexts = user_id.compactMap { user -> String? in
+            guard !visited.contains(user) else { return nil }
+            guard target.count == user.count else { return nil }
+            
+            guard target.enumerated().allSatisfy({ i, c in
+                let index = user.index(user.startIndex, offsetBy: i)
+                return c == "*" || c == user[index]
+            }) else { return nil }
+            return user
         }
         
+        
+        for next in nexts {
+            visited.insert(next)
+            var bag = bag
+            bag.insert(next)
+            dfs(depth + 1, bag)
+            visited.remove(next)
+        }
     }
-    
     dfs(0, [])
-    
     return answers.count
 }
