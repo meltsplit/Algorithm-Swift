1892fe2814dd9b466b2e82a4749f4a29bb3400bc
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-03-01 19:27:16 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 89.66 ms, Memory: 24.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.swift
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.swift
@@ -4,55 +4,34 @@ import Foundation
 // return: 가장짧은 구간의 시작 진열대 번호, 끝 진열대 번호
 
 func solution(_ gems:[String]) -> [Int] {
-    var count = Set(gems).count
+    var s = 0
+    var e = 0
+    var minDistance = Int.max
+    var answer = [0, gems.count - 1]
+    var dict = [String: Int]()
+    var target = Set(gems).count
     
-    var low = 0
-    var high = gems.count
-    var mid: Int { (low + high) / 2}
-    var answer = [Int:[Int]]()
+    dict[gems.first!] = 1
     
-    func isPossible(_ distance: Int) -> Bool {
-        var p1 = 0
-        var p2 = 0
-        var bag: [String: Int] = [:]
-        
-        for i in 0..<distance {
-            p2 = i
-            let newValue = gems[p2]
-            bag[newValue, default: 0] += 1
-        }
-        
-        while true {
-            // print(distance, p1, p2, bag)
-             if bag.keys.count == count { 
-                // print(bag.keys, count)
-                answer[distance] = [p1 + 1, p2 + 1]
-                return true
+    while e < gems.count && s <= e {
+        if dict.count == target {
+            let distance = e - s + 1
+            if distance < minDistance {
+                minDistance = distance
+                answer = [s, e]
             }
+            s += 1
             
-            let oldValue = gems[p1]
-            p1 += 1
-            p2 += 1
-            guard p2 < gems.count else { return false }
-            let newValue = gems[p2]
-            
-            if bag[oldValue] == 1 {
-                bag.removeValue(forKey: oldValue)
-            } else {
-                bag[oldValue]! -= 1
-            }
-            bag[newValue, default: 0] += 1
-        }
-        return false
-    }
-    
-    while low + 1 < high {
-        if isPossible(mid) {
-            high = mid
-        } else {
-            low = mid 
+            let oldValue = gems[s - 1]
+            dict[oldValue]! -= 1
+            if dict[oldValue] == 0 { dict.removeValue(forKey: oldValue) }
+        } else { // 미달 시 늘리기
+            e += 1
+            guard e < gems.count else { break }
+            let newValue = gems[e]
+            dict[newValue, default: 0] += 1
         }
     }
     
-    return answer[high] ?? [1, gems.count]
+    return answer.map { $0 + 1 }
 }
