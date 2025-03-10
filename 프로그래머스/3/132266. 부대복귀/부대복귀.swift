d2a4cf2d46f1fbf868c220d2bd7077f39d2b8527
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-03-10 16:55:53 +0900

    [level 3] Title: 부대복귀, Time: 1136.94 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.swift
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.swift
@@ -1,33 +1,59 @@
-import Foundation 
+import Foundation
 
-func solution(_ n:Int, _ roads:[[Int]], _ sources:[Int], _ destination:Int) -> [Int] {
-    var graph = [Int: [Int]]()
+// 1632
+// return: 최단시간
+
+struct Queue {
     
-    for road in roads {
-        graph[road[0], default: []].append(road[1])
-        graph[road[1], default: []].append(road[0])
+    var elements = [Int]()
+    var firstIndex = 0
+    
+    var isEmpty: Bool { firstIndex >= elements.count }
+    
+    mutating func append(_ element: Int) {
+        elements.append(element)
     }
     
-    var distance = Array(repeating: -1, count: n+1) // i 번째 지역에서 destination 까지의 최단 거리, 이 값은 destination에서 i번째 지역까지의 최단 거리로 생각 가능
-    distance[destination] = 0
-    
-    bfs(destination, graph, &distance)
-    
-    return sources.map { distance[$0] }
+    mutating func remove() -> Int? {
+        guard !isEmpty else { return nil }
+        let result = elements[firstIndex]
+        firstIndex += 1
+        return result
+    }
 }
 
-func bfs(_ destination: Int, _ graph: [Int: [Int]], _ distance: inout [Int]) {
-    var queue = [Int]()
-    queue.append(destination)
+func solution(_ n:Int, _ roads:[[Int]], _ sources:[Int], _ destination:Int) -> [Int] {
     
-    while !queue.isEmpty {
-        let cur = queue.removeFirst()
-        let nextNodes = graph[cur, default: []]
+    var graph = [Int: [Int]]()
+    
+    for i in 1...n {
+        graph[i] = []
+    }
+    
+    for road in roads {
+        graph[road[0]]!.append(road[1])
+        graph[road[1]]!.append(road[0])
+    }
+    
+    var distances = Array(repeating: -1, count: n + 1)
+    distances[destination] = 0
+    
+    func bfs() {
+        var queue = Queue()
+        queue.append(destination)
         
-        for node in nextNodes {
-            guard distance[node] == -1 else { continue }
-            distance[node] = distance[cur] + 1
-            queue.append(node)
+        while !queue.isEmpty {
+            guard let now = queue.remove() else { break }
+            let nexts = graph[now]!
+            
+            for next in nexts {
+                guard distances[next] == -1 else { continue } // -1 이여야 미방문 노드
+                distances[next] = distances[now] + 1
+                queue.append(next)
+            }
         }
     }
+    
+    bfs() 
+    return sources.map { distances[$0] }
 }
