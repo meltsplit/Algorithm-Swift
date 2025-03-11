17f0afa47af6afab1b3947e217851b2ee2ec0c23
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-03-11 19:23:04 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.19 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.swift
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.swift
@@ -1,43 +1,133 @@
 import Foundation
 
+struct Node {
+    let id: Int
+    let requestTime: Int
+    let interval: Int
+    
+    init(
+        _ id: Int,
+        _ requestTime: Int,
+        _ interval: Int
+    ) {
+        self.id = id
+        self.requestTime = requestTime
+        self.interval = interval
+    }
+    
+    static let root = Self(Int.min, Int.min, Int.min)
+}
+
+extension Node : Comparable {
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        guard lhs.interval == rhs.interval 
+        else { return lhs.interval > rhs.interval }
+        guard lhs.requestTime == rhs.requestTime 
+        else { return lhs.requestTime > rhs.requestTime }
+        return lhs.id > rhs.id
+    }
+}
+struct Heap {
+    
+    var elements: [Node]
+    
+    init () {
+        self.elements = [Node.root]
+    }
+    
+    var isEmpty: Bool { elements.count <= 1}
+    
+    func leftChild(_ index: Int) -> Int {
+        index * 2
+    }
+    
+    func rightChild(_ index: Int) -> Int {
+        index * 2 + 1
+    }
+    
+    func parent(_ index: Int) -> Int {
+        index / 2
+    }
+    
+    mutating func insert(_ node: Node) {
+        elements.append(node)
+        swimUp(elements.count - 1)
+    }
+    
+    mutating func remove() -> Node? {
+        guard !isEmpty else { return nil }
+        elements.swapAt(1, elements.count - 1)
+        let result = elements.removeLast()
+        diveDown(1)
+        return result
+    }
+    
+    mutating func diveDown(_ index: Int) {
+        var priorityIndex = index
+        let leftIndex = leftChild(index)
+        let rightIndex = rightChild(index)
+        
+        
+        
+        
+        if leftIndex < elements.count
+        && elements[leftIndex] > elements[priorityIndex] {
+            priorityIndex = leftIndex
+        }
+        
+        if rightIndex < elements.count
+        && elements[rightIndex] > elements[priorityIndex] {
+            priorityIndex = rightIndex
+        }
+        
+        if priorityIndex != index {
+            elements.swapAt(priorityIndex, index) 
+            diveDown(priorityIndex)
+        } 
+        
+    }
+    
+    mutating func swimUp(_ index: Int) {
+        let parentIndex = parent(index)
+        
+        if elements[index] > elements[parentIndex] {
+            elements.swapAt(index, parentIndex)
+            swimUp(parentIndex)
+        }
+    }
+    
+    
+}
+
 func solution(_ jobs:[[Int]]) -> Int {
+    let count = jobs.count
+    var jobs = jobs.enumerated().sorted { a, b in a.1[0] > b.1[0] }.map { Node($0, $1[0], $1[1]) }
+    var heap = Heap()
+    var now = 0
+    var answer = 0
     
-    var waits = jobs.sorted { $0[0] < $1[0] }
-    print(waits)
-    var able: [[Int]] = []
-    var time = 0
-    var answer: [Int] = []
-    
-    while !waits.isEmpty || !able.isEmpty { // 반복 wait와 able 모두 사라질때까지
-        
-        
-        while !waits.isEmpty {
-            let requestAt = waits.first![0]
-            let takes = waits.first![1]
-            
-            if requestAt <= time {
-                // print("뽑은 시점: \(requestAt)")
-                waits.removeFirst()
-                able.append([requestAt, takes])
-            } else {
-                // print("흥흥")
+    while !(jobs.isEmpty && heap.isEmpty) {
+        while !jobs.isEmpty {
+            let task = jobs.removeLast()
+            if task.requestTime <= now  {
+                heap.insert(task)
+            } else {  
+                jobs.append(task)
                 break
             }
         }
         
-        // print("able은 \(able)")
-        able = able.sorted { $0[1] < $1[1] }
-        // print("정렬된 able은 \(able)")
-        
-        if !able.isEmpty {
-            time += able.first![1]
-            answer.append(time-able.first![0])
-            able.removeFirst()
-        } else {
-            time += 1    
+        // 힙에서 값을 꺼낸다.
+        if let task = heap.remove() {
+            // print("\(now) 일 때 \(task)를 꺼냈습니다.")
+            now += task.interval
+            let result = now - task.requestTime
+            // print("반환 시간: \(now) - \(task.requestTime) = \(result)")
+            answer += result
+        } else if let targetTime = jobs.last?.requestTime {
+            now = targetTime
         }
-        
-    }   
+    }
     
-    return answer.reduce(0, +) / answer.count
+    return answer / count
 }
