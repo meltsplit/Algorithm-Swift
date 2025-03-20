6ebd5404ab04b77a3b86a3b46d3dbd86b2c865bb
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-03-20 21:02:41 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 29.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.swift
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.swift
@@ -1,63 +1,57 @@
 import Foundation
 
-// 13:44
+// 2020
 
-// order: 각 손님들이 주문한 단품메뉴들이 문자열 형식으로 담긴 배열
-// course: 코스요리를 구성하는 단품메뉴들의 갯수가 담긴 배열
+// Idea 1:
+// 모든 조합 = 
 
-// solution: 새로 추가하게 될 코스요리의 메뉴 구성을 문자열 형태로 배열
+// Idea 2: dp
+// f(x) =  x 번 나온 c개 묶음 조합
+// return: 스카피가 추가할 메뉴 구성
 
-// 문제
-// 가장 많이 함께 주문한 단품메뉴들을 코스요리 메뉴로 구성
-// 코스요리 메뉴는 최소 2가지 이상의 단품메뉴 
-// 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합에 대해서만 코스요리 메뉴 후보에 포함
-
-
-// Idea 1: 조합
-// 두개이상 주문이 있는 메뉴: Set<String>
-// Set의 combination 2, 3, 4
-// 해당 조합이 두번 이상나왔다면 append
-// answer.sorted()
-
-func combinations(_ arr: [String], 
-                  _ now: [String], 
-                  _ index: Int, 
-                  _ k: [Int],
-                  _ result: inout [String]) {
-        if k.contains(now.count) {
-            result.append(now.joined())
+func combination(_ elements: [String],  _ k: Int) -> [[String]] {
+    var result = [[String]]()
+    
+    func combi(_ index: Int, _ now: [String]) {
+        if now.count == k { 
+            result.append(now)
+            return
         }
         
-        for i in index..<arr.count {
-            combinations(arr, now + [arr[i]], i + 1, k, &result)
-        }
-}   
-
-func solution(_ orders:[String], _ course:[Int]) -> [String] {
-    
-    var combi: [String: Int] = [:]
-    
-    orders.forEach {
-        let orderedSingleMenus = $0.map { String($0) }
-        var menuSet: [String] = []
-        combinations(orderedSingleMenus, [], 0, course, &menuSet)
-        menuSet.forEach { menu in
-            let menu = String(menu.sorted())
-            combi[menu, default: 0] += 1
+        for i in index..<elements.count {
+            combi(i + 1, now + [elements[i]])
         }
     }
+    
+    combi(0, [])
+    return result
+}
 
+func solution(_ orders:[String], _ course:[Int]) -> [String] {
+    let orders = orders.map { Set($0.map { String($0)})  }
     var answer = [String]()
+    var alpha = Set(orders.flatMap { $0.map { String($0) } })
     
     for c in course {
-        var arr = combi.filter { $0.key.count == c }
-        let max = arr.values.max()
-        if max == 1 { continue }
-        let able = arr.filter { $0.value == max }.keys
-        answer.append(contentsOf: able)
+        var result = [Set<String>: Int]()
+
+        for order in orders {
+            let combis = combination(Array(order), c)
+            for combi in combis {
+                result[Set(combi), default: 0] += 1
+            }
+        }
+        
+        guard let maxValue = result.values.max(),
+        maxValue >= 2 else { continue }
+        
+        
+        for key in result.filter { $0.value == maxValue }.keys {
+            answer.append(key.sorted().joined())
+        }
+
     }
     
     
     return answer.sorted()
 }
-
