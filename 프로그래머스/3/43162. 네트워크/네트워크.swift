14fdb2595a9932191a63e3ed7c0e30ab20e61595
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2025-07-06 15:06:59 +0900

    [level 3] Title: 네트워크, Time: 0.36 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -1,44 +1,29 @@
 import Foundation
 
-
 // return: 네트워크 개수
-
-// Idea 1: dfs
-// 방문 처리 하면서, 노드가 끝났을 때 answer + 1
-// O(n ^ 2)
+ 
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     
-    var parent = (0..<n).map { $0 }
-    
-    func find(_ a: Int) -> Int {
-        guard parent[a] != a else { return a }
-        parent[a] = find(parent[a])
-        return parent[a]
+    var parents = (0..<n).map { $0 }
+
+    func find(_ x: Int) -> Int {
+        if parents[x] == x { return x }
+        parents[x] = find(parents[x])
+        return parents[x]
     }
-    
-    func union(_ a: Int, _ b: Int) {
-        let parentA = find(a)
-        let parentB = find(b)
-        
-        if parentA <= parentB { // b가 a의 부모를 따라가야 함
-            parent[parentB] = parentA
-        } else {
-            parent[parentA] = parentB
-        }
+
+    func union(_ x: Int, _ y: Int) {
+        parents[find(x)] = find(y)
     }
     
     for i in 0..<n {
         for j in (i + 1)..<n {
             guard computers[i][j] == 1 else { continue }
-            print(i,j,"병합")
             union(i,j)
-            print(parent)
         }
     }
     
-    for i in 0..<n {
-        find(i)
-    }
+    parents.indices.forEach { find($0) }
     
-    return Set(parent).count
+    return Set(parents).count
 }
